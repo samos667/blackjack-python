b2483b202964efb46154d6ebaab000d855466972
Sammy ETUR <sammyetur11@gmail.com>
2021-06-29 22:23:21 +0200

    update

--- blackjack.py
+++ blackjack.py
@@ -3,24 +3,21 @@ BlackJack en Python
 ASRBD 211 - Julien.G - Henri.N - Sammy.E
 """
 
-# Importing modules
-import random
+#Importing modules
+import random 
 from random import shuffle
 import os
 
-# Dictionary
+#Dictionary 
 dict_Values = {'2♠': 2, '3♠': 3, '4♠': 4, '5♠': 5, '6♠': 6, '7♠': 7, '8♠': 8, '9♠': 9, '10♠': 10, 'J♠': 10, 'Q♠': 10, 'K♠': 10, 'A♠': 11,
                '2♥': 2, '3♥': 3, '4♥': 4, '5♥': 5, '6♥': 6, '7♥': 7, '8♥': 8, '9♥': 9, '10♥': 10, 'J♥': 10, 'Q♥': 10, 'K♥': 10, 'A♥': 11,
                '2♦': 2, '3♦': 3, '4♦': 4, '5♦': 5, '6♦': 6, '7♦': 7, '8♦': 8, '9♦': 9, '10♦': 10, 'J♦': 10, 'Q♦': 10, 'K♦': 10, 'A♦': 11,
                '2♣': 2, '3♣': 3, '4♣': 4, '5♣': 5, '6♣': 6, '7♣': 7, '8♣': 8, '9♣': 9, '10♣': 10, 'J♣': 10, 'Q♣': 10, 'K♣': 10, 'A♣': 11}
 
-# This function creates the entire deck of 52 cards
-
-
+#This function creates the entire deck of 52 cards
 def deckCreation():
-    card_Types = ['♠', '♥', '♦', '♣']
-    card_Numbers = ['A', '2', '3', '4', '5',
-                    '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+    card_Types = ['♠','♥','♦','♣']
+    card_Numbers = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
     deck = []
 
     for i in card_Types:
@@ -28,22 +25,16 @@ def deckCreation():
             deck.append(j+i)
     return deck
 
-# This function draw 2 card
-
-
+#This function draw 2 card
 def drawCards():
     hand = [gameDeck.pop()]
     return hand
 
-# Function clear
-
-
+#Function clear
 def cls():
-    os.system('cls' if os.name == 'nt' else 'clear')
+    os.system('cls' if os.name=='nt' else 'clear')
 
-# Replay function
-
-
+#Replay function
 def playAgain():
     while True:
         print()
@@ -59,18 +50,17 @@ def playAgain():
             print("Error please do choice again")
     return play
 
-
 play = True
 
 while play == True:
 
-    # Preview of the generated deck
+    #Preview of the generated deck 
     gameDeck = deckCreation()
 
-    # Shuffle deck
+    #Shuffle deck
     shuffle(gameDeck)
 
-    # Draw cards to players
+    #Draw cards to players
     player_hand = []
     dealer_hand = []
     player_hand += drawCards()
@@ -78,11 +68,11 @@ while play == True:
     player_hand += drawCards()
     dealer_hand += drawCards()
 
-    # Calculate score
+    #Calculate score 
     playerScore = dict_Values[player_hand[0]] + dict_Values[player_hand[1]]
     dealerScore = dict_Values[dealer_hand[0]] + dict_Values[dealer_hand[1]]
 
-    # Display board
+    #Display board
     print("Welcome to BlackJack")
     print()
     print("Dealer's hand : "+dealer_hand[0]+" + Hidden card")
@@ -91,7 +81,7 @@ while play == True:
     print("Player's hand : "+player_hand[0]+" + "+player_hand[1])
     print("Player's score : "+str(playerScore))
 
-    # Condition for test blackjack situation = H
+    #Condition for test blackjack situation = H
     if playerScore == 21:
         if dealerScore == 21:
             print("Blackjack for all players. Draw !")
@@ -116,8 +106,7 @@ while play == True:
                     while dealerScore < playerScore:
                         dealer_hand += drawCards()
                         dealerScore += dict_Values[dealer_hand[-1]]
-                        print(
-                            "Dealers pick "+str(dealer_hand[-1])+" this score is "+str(dealerScore))
+                        print("Dealers pick "+str(dealer_hand[-1])+" this score is "+str(dealerScore))
                         if dealerScore > 21:
                             print("You win")
                             play = playAgain()
@@ -127,13 +116,11 @@ while play == True:
                             play = playAgain()
                             break
             elif choice.upper() == 'S':
-                print("Dealer's hand : " +
-                      str(dealer_hand[0])+" + "+str(dealer_hand[1]))
+                print("Dealer's hand : "+str(dealer_hand[0])+" + "+str(dealer_hand[1]))
                 while dealerScore <= playerScore:
                     dealer_hand += drawCards()
                     dealerScore += dict_Values[dealer_hand[-1]]
-                    print(
-                        "Dealers pick "+str(dealer_hand[-1])+" this score is "+str(dealerScore))
+                    print("Dealers pick "+str(dealer_hand[-1])+" this score is "+str(dealerScore))
                 if dealerScore > 21:
                     print("You win")
                     play = playAgain()
